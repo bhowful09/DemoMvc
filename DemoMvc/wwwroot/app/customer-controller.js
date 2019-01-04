ddcd81e29a26046ee557dc47051d1a89a88462b8
Benjamin Howard <benjamin.howard.09@gmail.com>
2019-01-03 20:32:02 -0500

    angular service changes

--- DemoMvc/wwwroot/app/customer-controller.js
+++ DemoMvc/wwwroot/app/customer-controller.js
@@ -1,46 +1,21 @@
-﻿angular.module('QuizApp', [])
-    .controller('QuizCtrl', function ($scope, $http) {
-        $scope.answered = false;
-        $scope.title = "loading customer...";
-        $scope.options = [];
-        $scope.correctAnswer = false;
+﻿angular.module('CustomersApp', ['customerService'])
+    .controller('CustomersCtrl', function ($scope, $http, CustomerService) {
+        $scope.title = "loading customers...";
         $scope.working = false;
-
-        $scope.answer = function () {
-            return $scope.correctAnswer ? 'correct' : 'incorrect';
-        };
-
-        $scope.nextQuestion = function () {
+        
+        $scope.getAllCustomers = function () {
             $scope.working = true;
-            $scope.answered = false;
             $scope.title = "loading customer...";
-            $scope.options = [];
 
-            $http.get("http://lazerpanther-001-site1.atempurl.com/api/customers/98").then(function (response) {
+            $http.get("http://lazerpanther-001-site1.atempurl.com/api/customers").then(function (response) {
                 $scope.title = "Customer";
                 $scope.working = true;
-                $scope.id = response.data.id;
-                $scope.firstName = response.data.firstName;
-                $scope.lastName = response.data.lastName;
-                $scope.city = response.data.city;
-                $scope.country = response.data.country;
-                $scope.phone = response.data.phone;
+                $scope.customers = response.data;
+                $scope.deleteme = CustomerService.addNums(response.data[0].firstName);
             }, function (error) {
                 $scope.title = "Oops... something went wrong";
                 $scope.working = false;
             });
-
-            $scope.sendAnswer = function (option) {
-                $scope.working = true;
-                $scope.answered = true;
-
-                $http.post('/api/trivia', { 'questionId': option.questionId, 'optionId': option.id }).success(function (data, status, headers, config) {
-                    $scope.correctAnswer = (data === true);
-                    $scope.working = false;
-                }).error(function (data, status, headers, config) {
-                    $scope.title = "Oops... something went wrong";
-                    $scope.working = false;
-                });
-            };
+            
         };
     });
---
+++ DemoMvc/wwwroot/app/customer-service.js
@@ -0,0 +1,10 @@
+﻿angular.module('customerService', [])
+    .service('CustomerService', function () {
+        this.addNums = function (text) {
+            return text + "123";
+        }
+
+        this.getAllCustomers = function () {
+            return '1';
+        }
+});
